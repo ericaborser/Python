e891737c05e7fc7326b3d3fe3e429768fd9b8098
Erica Borser <erica.borser.09@gmail.com>
2024-05-28 17:43:51 +0800

    Add files via upload

---
+++ Average of Grades.py
@@ -0,0 +1,11 @@
+#Average of 5 grades
+
+Fil=eval(input("Enter your grade in Filipino: "))
+Math=eval(input("Enter your grade in Math: "))
+Eng=eval(input("Enter your grade in English: "))
+Sci=eval(input("Enter your grade in Science: "))
+Comp=eval(input("Enter your grade in Computer: "))
+
+average=(Fil+Math+Eng+Sci+Comp)/5
+
+print("The average of your grades is ", average)
