38911a82e25c4f9e8af7d2423d32fb151f458bbf
Erica Borser <erica.borser.09@gmail.com>
2024-05-28 18:05:48 +0800

    Add files via upload

---
+++ Rounding Off.py
@@ -0,0 +1,11 @@
+#rounding off
+#round(num,place)
+#positive=decimal
+#negative=whole
+
+print(round(345678.141,-1))
+print(round(345678.141,-2))
+print(round(345678.141,-3))
+print(round(3.14156789,2))
+print(round(3.14156789,3))
+print(round(3.14156789,4))
