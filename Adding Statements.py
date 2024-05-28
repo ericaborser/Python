b2c2bd2dadb3028b7ad1de47e53a522af7942076
Erica Borser <erica.borser.09@gmail.com>
2024-05-28 18:00:57 +0800

    Add files via upload

---
+++ Adding Statements.py
@@ -0,0 +1,9 @@
+#Adding Statements
+
+favorite_food = "spaghetti"
+favorite_name = "my sister, Mikaela"
+favorite_drink = "apple juice"
+
+print("I like " + favorite_food + ".")
+print("I like eating " + favorite_food + " with " + favorite_name + ".")
+print("We also like to drink " + favorite_drink + ".")
