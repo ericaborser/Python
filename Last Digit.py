9e76f12f1a77852dc05ca98071aad50386d24e8c
Erica Borser <erica.borser.09@gmail.com>
2024-05-29 16:33:22 +0800

    Add files via upload

---
+++ Last Digit.py
@@ -0,0 +1,9 @@
+#Last Digit
+
+print("\n\t\t\tPROGRAM FOR NO.4.1")
+num1=eval(input("Enter the Whole Number: "))
+num2=eval(input("Enter the Power or Exponent: "))
+
+sol=(num1**num2)%10
+print("Last Digit: ",sol)
+
