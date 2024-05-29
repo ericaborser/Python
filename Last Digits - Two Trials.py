5fa08dd734aebbf28c84b752568d9c49d1ca5ba8
Erica Borser <erica.borser.09@gmail.com>
2024-05-29 16:38:45 +0800

    Add files via upload

---
+++ Last Digits - Two Trials.py
@@ -0,0 +1,15 @@
+print("\n\t\t\tPROGRAM FOR NO.4.1")
+num1=eval(input("Enter the Whole Number: "))
+num2=eval(input("Enter the Power or Exponent: "))
+
+sol=(num1**num2)%10
+print("Last Digit: ",sol)
+
+
+print("\n\t\t\tPROGRAM FOR NO.4.2")
+num1=eval(input("Enter the Whole Number: "))
+num2=eval(input("Enter the Power or Exponent: "))
+
+sol=(num1**num2)%100
+print("Last Digit: ",sol)
+
