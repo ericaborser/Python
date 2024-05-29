7b86ec0ce6b103273ff17ece3c3680df51166585
Erica Borser <erica.borser.09@gmail.com>
2024-05-29 15:48:31 +0800

    Add files via upload

---
+++ Area of the Triangle.py
@@ -0,0 +1,8 @@
+#Create a program that can calculate the area of a triangle
+
+print("AREA OF THE TRIANGLE")
+base=eval(input("BASE: "))
+height=eval(input("HEIGHT: "))
+
+area=0.5*(base*height)
+print("Area of the triangle: ",area)
