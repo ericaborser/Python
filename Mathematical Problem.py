cff95b4ddb5fafe661416a9fec7b21ca083a31fa
Erica Borser <erica.borser.09@gmail.com>
2024-05-29 16:14:57 +0800

    Add files via upload

---
+++ Mathematical Problem.py
@@ -0,0 +1,20 @@
+#Mathematical Problem
+
+a=3
+b=6
+c=0
+c=a+b
+print("Addition: ",c)
+c=a-b
+print("Subtraction: ",c)
+c=a*b
+print("Multiplication: ",c)
+c=b/a
+print("Division: ",c)
+c=b%a
+print("Modulus: ",c)
+c=a**b
+print("Power: ",c)
+c=9//2
+print("Integer Division: ",c)
+
