9a8f6be96edf1bfab0afe211e3e8223cd2baa905
Erica Borser <erica.borser.09@gmail.com>
2024-05-29 16:43:40 +0800

    Add files via upload

---
+++ Random Number - Randint.py
@@ -0,0 +1,14 @@
+#random numbers
+from random import randint, uniform
+
+#randint --- integer
+#uniform --- decimal   round(number,2)
+
+num1= randint(1,5)      #randint(minimum,maximum)
+num2= uniform(1,5)  
+
+print("Integer: ", num1)
+print("Decimal: ", round(num2,2))
+
+
+
